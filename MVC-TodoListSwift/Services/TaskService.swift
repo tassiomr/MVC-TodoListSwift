878f8be7481cfb0db8691c197dd926fa6c181db6
Tassio Rocha <=>
2020-05-29 10:40:35 -0300

    Create task service and task entity to Core Data

---
+++ MVC-TodoListSwift/Services/TaskService.swift
@@ -0,0 +1,24 @@
+//
+//  TaskService.swift
+//  MVC-TodoListSwift
+//
+//  Created by Tássio Marcos Rocha on 29/05/20.
+//  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
+//
+
+import UIKit
+
+class TaskService {
+
+    func create(task: Task) {}
+    
+    func delete(task: Task) {}
+    
+    func update(task: Task) {}
+    
+    func read() -> [Task] {
+        
+        return []
+    }
+    
+}
